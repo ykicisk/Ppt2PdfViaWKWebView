c36c305a388fd247bce5a5241831db0a4c34ac0d
ykicisk <ykic.p3@gmail.com>
2020-04-18 14:07:52 +0900

    Initial Commit

---
+++ Ppt2PdfViaWKWebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ppt2PdfViaWKWebView
+//
+//  Created by 井関洋平 on 2020/04/18.
+//  Copyright © 2020 Yohei Iseki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ppt2PdfViaWKWebViewTests/Ppt2PdfViaWKWebViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ppt2PdfViaWKWebViewTests.swift
+//  Ppt2PdfViaWKWebViewTests
+//
+//  Created by 井関洋平 on 2020/04/18.
+//  Copyright © 2020 Yohei Iseki. All rights reserved.
+//
+
+import XCTest
+@testable import Ppt2PdfViaWKWebView
+
+class Ppt2PdfViaWKWebViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
